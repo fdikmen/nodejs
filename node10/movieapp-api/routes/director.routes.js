bceaf622d8b8371186da67d9b152bc3d34309c09
frtdkmn <frtdkmn@gmail.com>
2021-03-07 00:22:09 +0300

    Added MovieApp Api Project Last Codes

--- node10/movieapp-api/routes/director.routes.js
+++ node10/movieapp-api/routes/director.routes.js
@@ -3,6 +3,23 @@ const router = express.Router()
 //Model
 const DirectorModel = require('../models/Director')
 
+const mongoose = require('mongoose')
+//=> /api/directors/:directorId
+router.get('/:directorId',(req,res,next)=>{
+    DirectorModel.aggregate([
+      { $match: { _id: mongoose.Types.ObjectId(req.params.directorId) } },
+      {
+        $lookup: {
+          from: "movies",
+          localField: "_id",
+          foreignField: "director_id",
+          as: "movies",
+        },
+      },
+    ])
+      .then((data) => {res.json(data);})
+      .catch((err) => {res.json(err);});
+})
 router.get('/',(req,res,next)=>{
     DirectorModel.aggregate([
         {
@@ -15,7 +32,7 @@ router.get('/',(req,res,next)=>{
         },
         {
             $project:{
-                _id:0,
+                _id:1,
                 name:1,
                 surname:true,
                 'movies.title':1,
@@ -26,12 +43,27 @@ router.get('/',(req,res,next)=>{
     .then((directorList)=>{res.json(directorList)})
     .catch((err)=>{next({message:err});})
 })
+
 router.post("/",(req,res,next)=>{
     const newDirector = new DirectorModel(req.body)
     newDirector.save()
                 .then((director)=>{res.json(director)})
                 .catch((err)=>{next({message:err});
                      /*res.json(err)*/})
+})
+
+//=> /api/directors/:directorId
+router.put('/:directorId',(req,res,next)=>{
+    DirectorModel.findByIdAndUpdate(req.params.directorId,req.body,{new:true})
+                        .then((director)=>{res.json(director)})
+                        .catch((err)=>{next({message:err});})
+})
+
+//=> /api/directors/:directorId
+router.delete('/:directorId',(req,res,next)=>{
+    DirectorModel.findByIdAndRemove(req.params.directorId)
+                        .then((director)=>{res.json(director)})
+                        .catch((err)=>{next({message:err});})
 })
 
 
